514f1f80adfc40a5b5cf5e0db42c722b74f249cc
Oleksandr Perlov <oleksandrperlov@gmail.com>
2022-10-11 13:44:11 +0300

    task-01 solution

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,83 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+galleryRef.addEventListener("click", onPictureClick);
+
+function createGalleryItems(items) {
+  return items
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+// ===============================================================================
+
+function onPictureClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const modal = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+  modal.show();
+
+  closeModalOnEscBtn(modal);
+}
+
+function closeModalOnEscBtn(instance) {
+  window.addEventListener("keydown", onEscBtnPress);
+
+  function onEscBtnPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscBtnPress);
+    }
+  }
+}
+
+// 2-й варіант ===========================================================================
+
+// function onPictureClick(event) {
+//   event.preventDefault();
+
+//   if (!event.target.classList.contains("gallery__image")) {
+//     return;
+//   }
+
+//   const modal = basicLightbox.create(`
+//       <img src="${event.target.dataset.source}" width="800" height="600">
+//   `);
+
+//   modal.show();
+
+//   const visible = modal.visible();
+
+//   if (visible) {
+//     window.addEventListener("keydown", onEscBtn);
+
+//     function onEscBtn(event) {
+//       if (event.code === "Escape") {
+//         modal.close();
+//         window.removeEventListener("keydown", onEscBtn);
+//       }
+//     }
+//   }
+// }
